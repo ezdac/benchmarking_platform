fb3b354aeaabb2f25cdf8fac530bbcdec928b192
Maximilian <maxdacjs@gmail.com>
2020-06-01 11:20:43 +0200

    Fix python2 not able to unpack list

--- scoring/fingerprint_lib.py
+++ scoring/fingerprint_lib.py
@@ -268,7 +268,9 @@ def TrainFP(fp_name, all_train_smiles, target):
     )
 
     REMOTE_FPS_PREFIXES.append(elements[1])
-    base_name = "_".join((str(target), *elements[1:4]))
+    name_elements = [str(target)]
+    name_elements = list(elements[1:4])
+    base_name = "_".join(name_elements)
 
     if len(elements) > 4:
         # this is a series fingerprint
